832476a36197c9cffac1878835a801e432b1e81e
vladislavB <my2b@ya.ru>
2016-09-30 13:24:45 +0300

    Implement arguments parser

--- lib/modules/ArgumentsParser.ts
+++ lib/modules/ArgumentsParser.ts
@@ -1,5 +1,13 @@
 /// <reference types="lodash" />
+import * as _ from "lodash"
 import {TSDocArguments} from "../TSDoc";
+
+const ARGUMENTS_PREFIX:string = '--';
+export const TSDOC_ARGUMENTS:Object = {
+    out: '--out',
+    help: '--help',
+    version: '--version'
+};
 
 interface ArgumentsParserInterface {
     parse():any;
@@ -8,8 +16,37 @@ interface ArgumentsParserInterface {
 export class ArgumentsParser implements ArgumentsParserInterface {
 
     public parse():TSDocArguments {
-        console.log(this.argv);
-        return {};
+        let currentArg:string;
+
+        return _(this.argv)
+            .transform((preparedArgs:TSDocArguments, argPart:string) => {
+                let currentArgValue:string|string[] = '';
+
+                if(_.startsWith(argPart, ARGUMENTS_PREFIX)){
+
+                    if(!_.includes(_.values(TSDOC_ARGUMENTS), argPart)){
+                        // TODO: make more verbose errors and handle them
+                        throw `Unexpected argument ${argPart}`;
+                    }
+
+                    currentArg = argPart;
+                } else if (!_.isEmpty(preparedArgs)){
+                    currentArgValue = <string|string[]>_.get(preparedArgs, currentArg) || argPart;
+
+                    if(_.isArray(currentArgValue)){
+                        currentArgValue.push(argPart);
+                    } else {
+                        currentArgValue = [currentArgValue];
+                    }
+                }
+
+                if(currentArg){
+                    _.set(preparedArgs, currentArg, currentArgValue);
+                }
+
+                return preparedArgs;
+            }, {})
+            .value();
     }
 
     constructor(private argv:string[]){
--- src/modules/ArgumentsParser.js
+++ src/modules/ArgumentsParser.js
@@ -1,11 +1,41 @@
 "use strict";
+var _ = require("lodash");
+var ARGUMENTS_PREFIX = '--';
+exports.TSDOC_ARGUMENTS = {
+    out: '--out',
+    help: '--help',
+    version: '--version'
+};
 var ArgumentsParser = (function () {
     function ArgumentsParser(argv) {
         this.argv = argv;
     }
     ArgumentsParser.prototype.parse = function () {
-        console.log(this.argv);
-        return {};
+        var currentArg;
+        return _(this.argv)
+            .transform(function (preparedArgs, argPart) {
+            var currentArgValue = '';
+            if (_.startsWith(argPart, ARGUMENTS_PREFIX)) {
+                if (!_.includes(_.values(exports.TSDOC_ARGUMENTS), argPart)) {
+                    throw "Unexpected argument " + argPart;
+                }
+                currentArg = argPart;
+            }
+            else if (!_.isEmpty(preparedArgs)) {
+                currentArgValue = _.get(preparedArgs, currentArg) || argPart;
+                if (_.isArray(currentArgValue)) {
+                    currentArgValue.push(argPart);
+                }
+                else {
+                    currentArgValue = [currentArgValue];
+                }
+            }
+            if (currentArg) {
+                _.set(preparedArgs, currentArg, currentArgValue);
+            }
+            return preparedArgs;
+        }, {})
+            .value();
     };
     return ArgumentsParser;
 }());
